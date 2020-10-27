8138759a0a4b679714a467e3853a2ca6ed8785ac
sergzaligin359 <webdevjsphp@gmail.com>
2020-10-27 10:13:27 +0300

    'fix'

---
+++ lesson_2/prepostarifmcycle.py
@@ -0,0 +1,12 @@
+# arithmetic cycle
+num = int(input('Pls input number: '))
+while num > 0:
+    print(f'N={num}')
+    num -= 1
+
+# pre cycle
+while True:
+    num = int(input('Pls input number 0-10: '))
+    if num > 10:
+        break
+print('Gracia!!!')
---
+++ lesson_2/recursion/recursion.py
@@ -0,0 +1,9 @@
+def func(a, b):
+    if a == b:
+        return f'{a}'
+    if a > b:
+        return f'{b} , {func(a, b + 1)}'
+    if a < b:
+        return f'{a} , {func(a + 1, b)}'
+
+print(func(1, 10))
